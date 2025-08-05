e9c2c3de2a549ea88284a1d4a0e2362e972cd53c
the white rabbit <khaledsaoud237@gmail.com>
2025-08-05 21:39:17 +0100

    advertising component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Hero";
 import Offers from "../components/Offers";
 import Products from "../components/Products";
+import Advertise from "../components/Advertise";
 
 const Home = () => {
   return (
@@ -9,6 +10,11 @@ const Home = () => {
       <Hero />
       <Offers />
       <Products />
+      <Advertise
+        img="https://static.thcdn.com/images/xlarge/webp/widgets/216-us/41/original-new_aa_desktop-112441.png"
+        text="Absolu Aventus"
+        desc="D'absolument irrésistible, Aventus Absolu est une interprétation audacieuse et moderne de l’icône Aventus. Un parfum qui incarne la puissance et la sophistication."
+      />
     </div>
   );
 };
