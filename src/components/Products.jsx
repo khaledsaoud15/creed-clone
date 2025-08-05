43f9b66628fe242be5c1f996d8bd8d1fd244085e
the white rabbit <khaledsaoud237@gmail.com>
2025-08-05 20:34:47 +0100

    products card

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,11 +5,11 @@ const Products = () => {
   return (
     <section className="h-fit w-full space-y-8 py-10 font-copperplate">
       <h2 className="text-2xl md:text-4xl px-4 md:px-8 ">Meilleures ventes</h2>
-      <grid className="grid grid-cols-2 w-full gap-6 lg:grid-cols-3">
+      <grid className="grid grid-cols-2 w-full gap-3 lg:grid-cols-3 lg:gap-4">
         {products.map((product) => (
           <div
             key={product.id}
-            className="flex flex-col items-center text-center p-4 gap-4 w-full"
+            className="flex flex-col items-center text-center p-4 gap-2 w-full lg:gap-3"
           >
             <img
               src={product.image}
@@ -17,8 +17,8 @@ const Products = () => {
               className="w-full  object-cover mb-4"
               loading="lazy"
             />
-            <h3 className="text-2xl ">{product.title}</h3>
-            <p className="text-gray-600 ">
+            <h3 className="text-xl lg:text-2xl ">{product.title}</h3>
+            <p className="text-gray-600 text-sm lg:text-base">
               A partir de ${product.price[0].toFixed(2)}
             </p>
             <button className="w-full py-3 bg-black text-white cursor-pointer">
