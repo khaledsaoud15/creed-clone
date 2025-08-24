d8dc37a707d2434d208ce5c80bb8b37f5d677115
the white rabbit <khaledsaoud237@gmail.com>
2025-08-24 07:03:54 +0100

    single product page

--- src/pages/Productpage.jsx
+++ src/pages/Productpage.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import Product from "../components/singleProduct/Product";
 import Navbar from "../components/Navbar";
 import Notes from "../components/singleProduct/Notes";
+import Reviews from "../components/singleProduct/Reviews";
 
 const Productpage = () => {
   return (
     <div>
       <Navbar />
       <section className="w-full h-fit  py-18 px-8 md:px-12 lg:px-24 space-y-25">
-        <Product />
-        <Notes />
+        <div className="space-y-0">
+          <Product />
+          <Notes />
+        </div>
+        <Reviews />
       </section>
     </div>
   );
